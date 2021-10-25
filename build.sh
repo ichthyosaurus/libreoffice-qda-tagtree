c3ec1268eb7e1cf61271d20063c0584a4392eb29
Mirian Margiani <mixosaurus+ichthyo@pm.me>
2021-10-25 21:13:40 +0200

    Add HSLuv color module

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 echo "packaging..."
-rm qda.oxt; zip -r qda.zip * -x build.sh _config.yml config.ini qda.oxt; mv qda.zip qda.oxt
+rm qda.oxt
+zip -r qda.zip * -x build.sh _config.yml config.ini qda.oxt \
+    TODO testdoc.* \
+    src/pythonpath/hsluv/{MANIFEST.in,README.md,setup.cfg,setup.py} \
+    src/pythonpath/hsluv/tests/* src/pythonpath/hsluv/tests \
+    '.*' '*/.*' '*~' '*/*~'
+mv qda.zip qda.oxt
 
 echo "removing old version..."
 unopkg remove qda.oxt
