d5386f002b565396d628791b220a4417ad3f1b60
Mirian Margiani <mixosaurus+ichthyo@pm.me>
2022-03-24 13:01:56 +0100

    Refactor build.sh

--- build.sh
+++ build.sh
@@ -3,20 +3,48 @@
 # SPDX-FileCopyrightText: 2021-2022 Mirian Margiani
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+if [[ "$1" == "-h" || "$1" == "--help" ]]; then
+    echo "usage: build.sh [install|install-test|i|it]"
+fi
+
+cBUILD_BASE="build"
+cBUILD="$cBUILD_BASE/qda-tagtree"
+
 echo "packaging..."
-rm qda.oxt
-zip -r qda.zip * -x build.sh _config.yml config.ini qda.oxt \
-    TODO testdoc.* \
-    src/pythonpath/hsluv/{MANIFEST.in,README.md,setup.cfg,setup.py} \
-    src/pythonpath/hsluv/tests/* src/pythonpath/hsluv/tests \
-    exclude/* exclude \
-    '.*' '*/.*' '*~' '*/*~'
-mv qda.zip qda.oxt
 
-echo "removing old version..."
-unopkg remove qda.oxt
+if [[ -e "$cBUILD_BASE" ]]; then
+    if ! mv "$cBUILD_BASE" "$cBUILD_BASE.old" --backup=t; then
+        echo "error: failed to remove old build directory"
+        exit 1
+    fi
+fi
 
-echo "installing new version..."
-unopkg add qda.oxt
+mkdir -p "$cBUILD_BASE"
 
-lowriter testdoc.odt
+cp -r qda-tagtree "$cBUILD"
+cp README.md "$cBUILD"
+rm -rf "$cBUILD"/src/pythonpath/hsluv/{MANIFEST.in,README.md,setup.cfg,setup.py,.git,.gitignore,.travis.yml}
+rm -rf "$cBUILD"/src/pythonpath/hsluv/tests
+
+find "$cBUILD" -iname "*~" -delete
+find "$cBUILD" -iname ".*" -delete
+
+back="$(pwd)"
+cd "$cBUILD"
+zip -r qda-tagtree.zip *
+
+cd "$back"
+mv "$cBUILD/qda-tagtree.zip" "$cBUILD_BASE/qda-tagtree.oxt"
+
+
+if [[ "$1" == "install"* || "$1" == "i"* ]]; then
+    echo "removing old version..."
+    unopkg remove qda.oxt
+
+    echo "installing new version..."
+    unopkg add qda.oxt
+fi
+
+if [[ "$1" == "install-test" || "$1" == "it" ]]; then
+    lowriter testdoc.odt
+fi
